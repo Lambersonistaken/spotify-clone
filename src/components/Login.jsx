648265104be76fdde23733c66f995ef1e1f5687a
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-05 02:27:10 +0300

    login page style added spotify credentials created

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,8 @@ function Login() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+
+      <a>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
