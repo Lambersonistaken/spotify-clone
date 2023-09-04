ecddec2e619fddda3a5e48f02a98752888018a5c
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-04 16:40:33 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/*Spotify Logo */}
+
+      {/*Login with Spotify Button */}
+      <Login />
+    </div>
+  );
 }
 
 export default App;
