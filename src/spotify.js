7e9253ecf192b4d7c688b48cfad789a4a857d915
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-05 15:52:55 +0300

    getting token from url

--- src/spotify.js
+++ src/spotify.js
@@ -15,3 +15,15 @@ const scopes = [
 export const loginURL = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
+
+export const getTokenFromURL = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      // #accessToken=mysupersecretkey&name=sonny
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
