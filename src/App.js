58614555dee0a1fbd6a4536663665df82fa6330e
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-05 17:43:59 +0300

    token stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import Player from "./components/Player";
+import { getTokenFromURL } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromURL();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("👱", user);
+      });
+    }
+  }, []);
+
+  return <div className="App">{token ? <h1>I am logged</h1> : <Login />}</div>;
 }
 
 export default App;
