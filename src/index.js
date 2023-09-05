ac21cdb747f643861aac56bcec2b91a6878351ef
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-05 23:17:28 +0300

    wrapped app component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DataLayer } from "./DataLayer";
+import { initialState } from "./reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DataLayer inital>
+    <DataLayer initialState={initialState} reducer>
       <App />
     </DataLayer>
   </React.StrictMode>
