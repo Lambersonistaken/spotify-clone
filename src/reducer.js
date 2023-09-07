c222788a74beca03cecc65365b3bc900e780b2e0
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-07 03:07:22 +0300

    set token reducer

--- src/reducer.js
+++ src/reducer.js
@@ -14,6 +14,12 @@ const reducer = (state, action) => {
         ...state,
         user: action.user,
       };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
     default:
       return state;
   }
