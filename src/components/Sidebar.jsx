acb6fd20a3e6dacc580fe5877e8b2ddb62e4ec01
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-10 18:02:12 +0300

    added material ui and icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 
 function Sidebar() {
   return (
@@ -10,9 +13,17 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <SidebarOption title="Home" />
-      <SidebarOption title="Search" />
-      <SidebarOption title="Your Library" />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+
+      <br />
+      <strong className="sidebar_title">PLAYLISTS</strong>
+      <hr />
+
+      <SidebarOption title="Hip hop" />
+      <SidebarOption title="Rock" />
+      <SidebarOption title="RnB" />
     </div>
   );
 }
