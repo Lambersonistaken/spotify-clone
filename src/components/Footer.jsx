6ea959fd4318be1ee2ae85fdc751431aa157bae3
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-14 18:50:24 +0300

    center and right footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import ReplayIcon from "@mui/icons-material/Replay";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import SidebarOption from "./SidebarOption";
+import { Grid, Slider } from "@mui/material";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer_left"></div>
-      <div className="footer_center"></div>
+      <div className="footer_left">
+        <img src="" alt="" />
+        <div className="footer_songInfo">
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+
+      <div className="footer_center">
+        <SidebarOption Icon={ShuffleIcon} title="Shuffle" />
+        <SidebarOption Icon={SkipPreviousIcon} title="Previous" />
+        <SidebarOption Icon={PlayCircleOutlineIcon} title="Play" />
+        <SidebarOption Icon={SkipNextIcon} title="Next" />
+        <SidebarOption Icon={ReplayIcon} title="Repeat" />
+      </div>
+
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
