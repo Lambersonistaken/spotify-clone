41699e4bd554e7b37b62e666791639d4bfabdceb
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-15 19:40:19 +0300

    header done last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import { Avatar } from "@mui/material";
+import { useDataLayerValue } from "../DataLayer";
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
+
   return (
     <div className="header">
       <div className="header_left">
@@ -14,8 +17,8 @@ function Header() {
         />
       </div>
       <div className="header_right">
-        <Avatar src="" alt="RQ" />
-        <h4>Username</h4>
+        <Avatar src={user?.images[0]?.url} alt="RQ" />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );
