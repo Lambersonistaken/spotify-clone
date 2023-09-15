8d5855084d45c73eb9f3296421b7050ad9106f7e
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-15 23:29:08 +0300

    body started

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,6 +6,18 @@ function Body({ spotify }) {
   return (
     <div className="body">
       <Header spotify={spotify} />
+
+      <div className="body_info">
+        <img
+          src="https://cms-fym.imgix.net/8cme_Qwug_TG_Wrx_G_Qvj2kf_51f36999b6.png?auto=compress,format&fit=fillmax&ch=Save-Data&w=1200&max-h=1200"
+          alt=""
+        />
+        <div className="body_infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>description...</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,8 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
+  token:
+    "BQD-JK_eJk793fAd2yRYx2o5FKVe9YwuubICTx6bIBYTZtVwulRHDqWjHMVdCFsFWUr9OPgUfCXMYchLun_WzVtNPQJvzrFuUVTq0XCGo9EA_Vmo4AXfnPJlz0Ov8gB4pf88WjWeFnqKxOxYAre3GjV9KYweu_Wgn5VLVgVTaz_sOeM1g77NnI3RoPjYkHGmKr4Gkvzm4qRhruOYQxZY",
 };
 
 const reducer = (state, action) => {
