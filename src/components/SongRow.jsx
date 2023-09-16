5a752edc887a7495c77996309ddaae5df56642f8
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-16 03:21:01 +0300

    app finished

--- src/components/SongRow.jsx
+++ src/components/SongRow.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "./SongRow.css";
 
-function SongRow({ track }) {
+function SongRow({ track, playSong }) {
+  console.log(track);
   return (
-    <div className="songRow">
+    <div className="songRow" onClick={() => playSong(track.id)}>
       <img className="songRow_album" src={track.album.images[0].url} alt="" />
-      <div className="songRoW_info">
+      <div className="songRow_info">
         <h1>{track.name}</h1>
         <p>
           {track.artists.map((artist) => artist.name).join(", ")} -{" "}
--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,6 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  token:
-    "BQBL6UnejdDnNYtdtlno2mExmOXTAvWs-wzZ6OUSU5N6Ni7iC8VPsOwfYE_2E_-BCSqhFUB0dm7DvR_cs_52QVW_4_XcBpw6yvkLjp8YVKQcs96G0pNq55MmJoOXkemedYAysfQ-_ACgylabCEJoPeEKw-rPhn2At6SducEET_H3jaebJxhBLJzSbJxRDotowS1RD0lcV0vDmbMPr4KR",
 };
 
 const reducer = (state, action) => {
